e6e039e399400b12c0d64a31d4ec1032729cb395
pboopathi <pboopathi@ebay.com>
2020-08-06 23:01:44 -0700

    Added my project

---
+++ index.js
@@ -0,0 +1,65 @@
+const SlackBot = require('slackbots');
+const axios = require('axios');
+
+const bot = new SlackBot({
+  token: 'xoxb-404612894081-1310882225040-GiluY0v8YbpMNvFKm2GAVwB2',
+  name: 'jokebot'
+});
+
+// Start Handler
+bot.on('start', () => {
+  const params = {
+    icon_emoji: ':smiley:'
+  };
+
+  bot.postMessageToChannel(
+    'general',
+    'Get Ready To Laugh With @Jokebot!',
+    params
+  );
+});
+
+//Error Handler
+bot.on('error', err => console.log(err))
+
+//Message Handler
+
+bot.on('message', data =>{
+
+  if(data.type !== 'message'){
+  return;
+  }
+  console.log(data);
+
+  handleMessage(data.text);
+})
+
+  //respond to data
+  function handleMessage(message){
+    if(message.includes(' chucknorris')){
+      chuckJoke();
+
+    }
+
+    }
+    //
+    function chuckJoke(){
+      axios.get('http://api.icndb.com/jokes/random/')
+      .then(res => {
+     const joke =   res.data.value.joke;
+     const params = {
+      icon_emoji: ':laughing:'
+    };
+  
+    bot.postMessageToChannel(
+      'general',
+      `Chuck Norries: ${joke}`,
+      params
+    );
+      })
+    }
+
+  
+
+  
+
