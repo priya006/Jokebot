7997b7ae0228ce4f7597f8fd37ca3ea9e49c1ab3
pboopathi <pboopathi@ebay.com>
2020-08-07 10:22:03 -0700

    webhook url added as fucntion in index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
 
-//https://api.slack.com/apps/A018893PTT8/oauth?success=1
+//https://api.slack.com/apps/A018893PTT8/oauth?success=1  app name is new
 const bot = new SlackBot({
-  token: 'xoxb-404612894081-1310882225040-HN3FoBCv5jUHte9ua1iYeJ66',
+  token: 'xoxb-404612894081-1310882225040-JIgcWdG7lsRyr7sHmnwyLJEg',
   name: 'jokebot'
 });
 
@@ -39,6 +39,7 @@ bot.on('message', data =>{
   function handleMessage(message){
     if(message.includes(' chucknorris')){
       chuckJoke();
+      webHook();
 
     }
 
@@ -60,7 +61,21 @@ bot.on('message', data =>{
       })
     }
 
-  
+    function webHook(){
 
-  
+    const options = {
+      text: "This is posted by webhook url_created by priya from index.js",
+    };
 
+axios.post('https://hooks.slack.com/services/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', JSON.stringify(options))
+      .then((response) => {
+        console.log('SUCEEDED: sent slack webhook: \n', response.data);
+        resolve(response.data);
+      })
+
+    .catch((error)  => {
+      console.log('FAILED send slack webhook', error);
+      PromiseRejectionEvent(new Error('FAILED: send slack webhook'));
+  
+    });
+  }
