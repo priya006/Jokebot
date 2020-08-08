5815dcdfca904b82a26182a95fd9cb4e89c0f858
pboopathi <pboopathi@ebay.com>
2020-08-07 22:34:16 -0700

    slack incoming messaging added message attachments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 //https://api.slack.com/apps/A018893PTT8/oauth?success=1  app name is new
 const bot = new SlackBot({
-  token: 'xoxb-404612894081-1310882225040-K7vWzxaJ8soBIvAT6asfgVmv',
+  token: 'xoxb-404612894081-1310882225040-0n2GXOdsDmxW6PBTFksADWKH',
   name: 'jokebot'
 });
 
@@ -64,11 +64,47 @@ bot.on('message', data =>{
     function webHook(){
 
     const options = {
-      text: "This is posted by webhook url_created by priya from index.js",
+      
+       text: "Webhook incoming message",
+        "attachments": [
+            {
+                "mrkdwn_in": ["text"],
+                "color": "#36a64f",
+                "pretext": "Optional pre-text that appears above the attachment block",
+                "author_name": "author_name",
+                "author_link": "http://flickr.com/bobby/",
+                "author_icon": "https://placeimg.com/16/16/people",
+                "title": "title",
+                "title_link": "https://api.slack.com/",
+                "text": "Optional `text` that appears within the attachment",
+                "fields": [
+                    {
+                        "title": "A field's title",
+                        "value": "This field's value",
+                        "short": false
+                    },
+                    {
+                        "title": "A short field's title",
+                        "value": "A short field's value",
+                        "short": true
+                    },
+                    {
+                        "title": "A second short field's title",
+                        "value": "A second short field's value",
+                        "short": true
+                    }
+                ],
+                "thumb_url": "http://placekitten.com/g/200/200",
+                "footer": "footer",
+                "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                "ts": 123456789
+            }
+        ]
+    
     };
 
     //https://api.slack.com/apps/A018893PTT8/incoming-webhooks?success=1 Click Add new webhook to workspace
-axios.post('https://hooks.slack.com/services/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', JSON.stringify(options))
+axios.post('https://hooks.slack.com/services/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', JSON.stringify(options))
       .then((response) => {
         console.log('SUCEEDED: sent slack webhook: \n', response.data);
         resolve(response.data);
